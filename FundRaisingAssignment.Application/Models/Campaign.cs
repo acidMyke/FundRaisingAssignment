0bd34020c83e6a8e3d57ff7dd8b5a854ac6869fe
acidMyke <owner@acidmyke.link>
2026-04-25 18:24:01 +0800

    fix: change campaign primary id to be Guid

--- FundRaisingAssignment.Application/Models/Campaign.cs
+++ FundRaisingAssignment.Application/Models/Campaign.cs
@@ -4,7 +4,7 @@ namespace FundRaisingAssignment.Application.Models
 {
     public class Campaign
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 
         [Required]
         [StringLength(100)]
