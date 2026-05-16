e701daccd46d4e9615215d61377af0617a533fa9
acidMyke <owner@acidmyke.link>
2026-05-17 00:33:40 +0800

    fix: email link

--- FundRaisingAssignment.Test/CampaignDigestEmailTemplateTests.cs
+++ FundRaisingAssignment.Test/CampaignDigestEmailTemplateTests.cs
@@ -73,6 +73,6 @@ public class CampaignEmailDigestTemplateServiceTests
         var html = _service.RenderHtmlBody(viewModel);
 
         // Assert
-        Assert.Contains($"https://givehive.acidmyke.link//Dashboard/CampaignPage/{campaignId}", html);
+        Assert.Contains($"https://givehive.acidmyke.link/Dashboard/CampaignPage/{campaignId}", html);
     }
 }
